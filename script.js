f2e0f2e7b52b7e32f69a327b14c29d16354b71d6
Hao <faraway520@gmail.com>
2021-10-26 14:23:59 +0800

    update

--- script.js
+++ script.js
@@ -19,24 +19,15 @@ function staticLoadPlaces() {
 }
 
 var models = [
+
     {
         url: './assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
     },
-    {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
-    },
 
-    {
-        url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
-        rotation: '0 180 0',
-        info: 'Dragonite, Lv. 99, HP 150/150',
-    },
+
+
 ];
 
 var modelIndex = 0;

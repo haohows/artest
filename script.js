3cdf8422c8d83847f83d8273a0b92c6f75344d50
Hao <faraway520@gmail.com>
2021-10-26 16:20:41 +0800

    update

--- script.js
+++ script.js
@@ -32,35 +32,25 @@
 
 let places = [
     {
-        lat: "25.035050308213247",
-        lng: "121.43170121737388",
-        name: "1號"
+        lat: "25.035342443040633",
+        lng: "121.43148178559154",
+        name: "聖言樓"
     },
     {
-        lat: "25.035205841744904",
-        lng: "121.43149736950528",
-        name: "2號"
+        lat: "25.0359312591485",
+        lng: "121.43142559368194",
+        name: "百鍊廳"
     },
     {
-        lat: "25.035338288112488",
-        lng: "121.4314946872965",
-        name: "3號"
+        lat: "25.035920742042894",
+        lng: "121.4305759064258",
+        name: "濟時樓"
     },
     {
-        lat: "25.03548410046189",
-        lng: "121.4315764946648",
-        name: "4號"
+        lat: "25.035775605747382",
+        lng: "121.43097056987368",
+        name: "理園"
     },
-    {
-        lat: "25.035284823540945",
-        lng: "121.43182594008293",
-        name: "5號"
-    },
-    {
-        lat: "25.035176679222726",
-        lng: "121.43174010940142",
-        name: "6號"
-    }
 ];
 
 
@@ -81,7 +71,7 @@ window.onload = () => {
         const placeText = document.createElement('a-link');
         placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         placeText.setAttribute('title', place.name);
-        placeText.setAttribute('scale', '15 15 15');
+        placeText.setAttribute('scale', '30 30 30');
 
         placeText.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))

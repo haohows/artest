e1e05bc71447d74eb18b27e866aa2b9e9c08f42f
hao <faraway520@gmail.com>
2021-10-27 23:57:12 +0800

    更新

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let places = [
             lat: "25.038324123400365",
             lng: "121.42251495236201",
         },
-        name: "bbb",
+        name: "7-11",
         img: "./assets/img/mYmmbrp.jpg"
     },
     // 25.038456438143236, 121.42154768226726
@@ -15,7 +15,7 @@ let places = [
             lat: "25.038456438143236",
             lng: "121.42154768226726",
         },
-        name: "ccc",
+        name: "TM",
         img: "./assets/img/wooden.png"
     },
 ];
@@ -28,22 +28,22 @@ window.onload = () => {
             const latitude = place.location.lat;
             const longitude = place.location.lng;
 
-            const placeText = document.createElement('a-link');
-            placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-            placeText.setAttribute('title', place.name);
-            placeText.setAttribute('scale', '15 15 15');
-
-            // const placeText = document.createElement('a-box');
+            // const placeText = document.createElement('a-link');
             // placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-            // placeText.setAttribute('src', place.img);
-            // placeText.setAttribute('position', "0 0 -4");
-            // placeText.setAttribute('rotation', "0 45 45");
-            // placeText.setAttribute('scale', "1 1 1");
-            // placeText.setAttribute('animation', "property: rotation;from: 0 0 0; to: 0 360 0; dir: normal; dur: 20000; loop: true;easing:linear");
-            // placeText.setAttribute('animation__2', "property: object3D.position.y; to: 0.3; dir: alternate; dur: 2000; loop: true");
-            // placeText.setAttribute('animation__3', "property: rotation;startEvents: click;; to: 0 420 420; dir: alternate; dur: 2500; easing:easeOutCubic;");
-            // placeText.setAttribute('animation__4', "property: scale;startEvents: click; to: 0 0 0; dir: alternate;  dur: 500; easing:linear;delay: 1000");
-            // placeText.setAttribute('animation__5', "property: rotation;startEvents: mouseleave; to: 0 360 0; dir: normal; dur: 20000; loop: true;easing:linear");
+            // placeText.setAttribute('title', place.name);
+            // placeText.setAttribute('scale', '15 15 15');
+
+            const placeText = document.createElement('a-box');
+            placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+            placeText.setAttribute('src', place.img);
+            placeText.setAttribute('position', "0 0 -4");
+            placeText.setAttribute('rotation', "0 45 45");
+            placeText.setAttribute('scale', "1 1 1");
+            placeText.setAttribute('animation', "property: rotation;from: 0 0 0; to: 0 360 0; dir: normal; dur: 20000; loop: true;easing:linear");
+            placeText.setAttribute('animation__2', "property: object3D.position.y; to: 0.3; dir: alternate; dur: 2000; loop: true");
+            placeText.setAttribute('animation__3', "property: rotation;startEvents: click;; to: 0 420 420; dir: alternate; dur: 2500; easing:easeOutCubic;");
+            placeText.setAttribute('animation__4', "property: scale;startEvents: click; to: 0 0 0; dir: alternate;  dur: 500; easing:linear;delay: 1000");
+            placeText.setAttribute('animation__5', "property: rotation;startEvents: mouseleave; to: 0 360 0; dir: normal; dur: 20000; loop: true;easing:linear");
 
             placeText.addEventListener('loaded', () => {
                 window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
